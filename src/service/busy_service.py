0b8859be4d469280695c79e05962a16eeadf7c07
Sahil <sahilkumar1129@gmail.com>
2021-08-24 09:25:33 -0400

    Busy api

---
+++ src/service/busy_service.py
@@ -0,0 +1,63 @@
+from flask import request, jsonify
+import datetime
+from src.app import db
+from src.models.barbusyness import BarBusyness
+from src.models.bar import Bar
+from src.models.location import Location
+from src.models.neighborhood import Neighborhood
+from src.models.busyness import Busyness
+from src.service.neighborhood_service import NeighborhoodService as neighborhood_service
+from src.service.bar_service import BarService as bar_service
+
+
+class BusyService():
+    
+    def create_busy_bar(self, body):
+        location = Location.query.filter_by(location=body['location']).first()
+        if (body['neighborhood']) is not None:
+            neighborhood = Neighborhood.query.filter_by(neighborhood=body['neighborhood'].lower()).first()
+            if neighborhood is None:
+                neighborhood = neighborhood_service().create_neighborhood(location_id=location.id, neighborhood=body['neighborhood'])
+            bar = Bar.query.filter_by(location_id=location.id, neighborhood_id=neighborhood.id, name=body['bar'].lower()).first()
+            if bar is None:
+                bar = bar_service().create_bar(bar_name=body['bar'], location_id=location.id, neighborhood_id=neighborhood.id)
+        else:
+            bar = Bar.query.filter_by(location_id=location.id, name=body['bar'].lower()).first()
+            if bar is None:
+                bar = bar_service().create_bar(bar_name=body['bar'], location_id=location.id)
+        day_of_week_id = datetime.datetime.now().isoweekday()
+        hour = datetime.datetime.now().hour
+        minute = datetime.datetime.now().minute
+        if minute > 30:
+            start_hour = hour + 1
+        else:
+            start_hour = hour
+        end_hour = start_hour + 1
+        google_live_busyness = body['google_live_busyness']   
+        busyness = Busyness.query.filter_by(busyness=body['busyness']).first()
+        busyness_id = busyness.id
+        google_average_busyness = None 
+        if (body['google_average_busyness']):
+           google_average_busyness = body['google_average_busyness']
+        busy_bar = BarBusyness(bar_id=bar.id, day_of_week_id=day_of_week_id, start_hour=start_hour, end_hour=end_hour, busyness_id=busyness.id, google_average_busyness_id=google_average_busyness, google_live_busyness_id=google_average_busyness)
+        db.session.add(busy_bar)
+        db.session.commit()
+        
+
+    def get_live_busy(self, body):
+        location = Location.query.filter_by(location=body['location']).first()
+        if (body['neighborhood']) is not None:
+            neighborhood = Neighborhood.query.filter_by(neighborhood=body['neighborhood'].lower()).first()
+            bar = Bar.query.filter_by(location_id=location.id, neighborhood_id=neighborhood.id, name=body['bar'].lower())
+        else:
+            bar = Bar.query.filter_by(location_id=location.id, name=body['bar'].lower())
+         
+    
+    def get_average_busy(self, body):
+        location = Location.query.filter_by(location=body['location']).first()
+        if (body['neighborhood']) is not None:
+            neighborhood = Neighborhood.query.filter_by(neighborhood=body['neighborhood'].lower()).first()
+            bar = Bar.query.filter_by(location_id=location.id, neighborhood_id=neighborhood.id, name=body['bar'].lower())
+        else:
+            bar = Bar.query.filter_by(location_id=location.id, name=body['bar'].lower())
+         
---
+++ src/service/neighborhood_service.py
@@ -0,0 +1,12 @@
+from src.app import db
+from src.models.neighborhood import Neighborhood
+from uuid import uuid4
+
+class NeighborhoodService():
+
+    def create_neighborhood(self, location_id, neighborhood):
+        nbhood_uuid = str(uuid4())
+        nbhood= Neighborhood(uuid=nbhood_uuid, location_id=location_id, neighborhood=neighborhood.lower())
+        db.session.add(nbhood)
+        db.session.commit()
+        return Neighborhood.query.filter_by(uuid=nbhood_uuid, neighborhood=neighborhood.lower(), location_id=location_id).first()
